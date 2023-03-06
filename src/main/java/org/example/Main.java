416212a4f9933a0af39207d6b36067dd3aa6dd3a
bw1611 <97327583+leebuwon@users.noreply.github.com>
2023-03-06 14:22:56 +0900

    1단계 : 종료기능, Tdd 개발 (#4)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.example.wiseSaying.App;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        new App(new Scanner(System.in)).run();
     }
 }
