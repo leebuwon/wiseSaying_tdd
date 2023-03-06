5fd7e2f615a00673c1644a54e10acb130333d87e
bw1611 <bw1611@naver.com>
2023-03-06 14:21:34 +0900

    1단계 : 종료기능, Tdd 개발

--- src/main/java/org/example/wiseSaying/App.java
+++ src/main/java/org/example/wiseSaying/App.java
@@ -1,7 +1,32 @@
 package org.example.wiseSaying;
 
-public class App {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+
+            String cmd = sc.nextLine().trim();
+
+            if (cmd.isEmpty()) continue;
+
+            switch (cmd) {
+                case "종료":
+                    System.out.println("명언 앱이 종료되었습니다.");
+                    return;
+                default:
+                    System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
+                    break;
+            }
+        }
     }
 }
