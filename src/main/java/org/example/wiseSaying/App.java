8db6b139e63ff8b8833f060fb1d703e2edd390e0
bw1611 <bw1611@naver.com>
2023-03-06 15:33:37 +0900

    5단계 : 목록으로 가져오기

--- src/main/java/org/example/wiseSaying/App.java
+++ src/main/java/org/example/wiseSaying/App.java
@@ -1,14 +1,18 @@
 package org.example.wiseSaying;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private final Scanner sc;
+    private final List<WiseSaying> wiseSayingList;
 
     private long cnt;
 
     public App(Scanner sc) {
         cnt = 0;
+        wiseSayingList = new ArrayList<>();
         this.sc = sc;
     }
 
@@ -27,15 +31,24 @@ public class App {
                     System.out.println("명언 앱이 종료되었습니다.");
                     return;
                 case "등록":
+                    long id = cnt + 1;
                     System.out.print("명언 : ");
                     String content = sc.nextLine().trim();
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
-
-                    long id = cnt + 1;
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayingList.add(wiseSaying);
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     cnt = id;
+                    break;
+                case "목록":
+                    System.out.println("번호 / 명언 / 작가");
+                    System.out.println("-----------------------------");
+                    for (int i = wiseSayingList.size() - 1; i >= 0; i--){
+                        WiseSaying wiseSayings = wiseSayingList.get(i);
+                        System.out.println(wiseSayings);
+                    }
                     break;
                 default:
                     System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
